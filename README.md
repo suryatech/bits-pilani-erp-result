7d7d1f8190ee592a6475f889594a5ea1e8b30c17
Surya P <suryasongs@gmail.com>
2014-12-19 16:49:59 +0530

    Factored out term and userid, updated readme file

---
+++ README.md
@@ -0,0 +1,24 @@
+# BITS Pilani ERP Result #
+Poll the BITS Pilani ERP server for semester results and send an email when they are out.
+### Usage
+
+ - `npm install` once you are in the root directory of the project
+ 
+ - Edit the `config.json` file
+	  - `access_token:` Add a valid FB access token to post on a facebook group (optional)
+	  - `gmail_username/gmail_password`: Add a gmail username and password to send an email
+	  - `destination_email`: Add a destination email to receive the email when the results are out
+	  - `smtpServer`: Mail Server to use
+	  - `group_id`: FB group to publish a message
+	  - `username`: ERP username
+	  - `password:` ERP Password
+	  - `interval`: Check for the results after every x milliseconds. (The session cookies expire after about 5 minutes. It is not a very good idea to login every 5 minutes to check the result. Set it to a value < 5 minutes to avoid logging in every single time)
+
+ - Edit the `index.js` file
+	  - `userid`: Numeric ID corresponding to the username field in config.json. For ex: 31120110XXX, for username: 2011C6PSXXXG.
+	  - `term`: Semester ID. This can be found in the grades section in your account.
+	  - Edit the facebook post message and the email text as required.
+	  
+ - Set the `log4js` output file
+
+ - `node index.js`
--- index.js
+++ index.js
@@ -2,14 +2,20 @@ var request = require('request'),
     cheerio = require('cheerio'),
     config = require('./config.json'),
     log4js = require('log4js'),
+    jar = request.jar();
 
-    jar = request.jar(),
+var userid = 31120110480,
+    term = 1130,
     loginURL = 'https://erp.bits-pilani.ac.in:4431/psp/hcsprod/?cmd=login&languageCd=ENG',
-    gradeBookURL = 'https://erp.bits-pilani.ac.in:4431/psc/hcsprod/EMPLOYEE/HRMS/c/SA_LEARNER_SERVICES.SSR_SSENRL_GRADE.GBL?ACAD_CAREER=0001&EMPLID=31120110480&INSTITUTION=BITS&STRM=1130',
+    gradeBookURL = 'https://erp.bits-pilani.ac.in:4431/psc/hcsprod/EMPLOYEE/HRMS/c/SA_LEARNER_SERVICES.SSR_SSENRL_GRADE.GBL?ACAD_CAREER=0001&EMPLID='+ userid + '&INSTITUTION=BITS&STRM=' + term,
     facebookURL = 'https://graph.facebook.com/',
+
+    // Edit the message as required.
     messageString = 'ARC has uploaded the grades of Semester 1, 2014-15 onto \
                     the ERP server. Click below to check your grades.\n\n\
                     Sent via ERP Result Notifier',
+
+    // User Agent string.
     ua = 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36',
     ssl = 'SSLv3_method',
     $ = '',
@@ -21,6 +27,7 @@ var request = require('request'),
         host: config.smtpServer,
         ssl: true
     }),
+
     mailMessage = {
         from: "ERP Result Notifier<" + config.gmail_username + ">",
         subject: "Semester 1, 2014-15 Grades",
@@ -45,6 +52,7 @@ var logger = log4js.getLogger('info');
 // Send a pre-defined email to destination_email when the results are out.
 var sendMail = function() {
 
+    // Edit the email text as required.
     mailMessage.text = "Grades Updated.";
 
     mailServer.send(mailMessage, function(err, message) {
@@ -125,7 +133,7 @@ var checkGrade = function() {
         }
 
         $ = cheerio.load(body);
-        
+
         // GPA Element
         var gpa = $("#STATS_ENRL\\$12").text().trim();
 
